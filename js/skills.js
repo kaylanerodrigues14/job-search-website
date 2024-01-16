4732d3e75edda156f78489e60411b8bfc2ae43d1
kaylanerodrigues14 <148080549+kaylanerodrigues14@users.noreply.github.com>
2024-01-16 20:26:03 +0000

    Update 3 files

--- js/skills.js
+++ js/skills.js
@@ -1,26 +1,102 @@
 $(document).ready(function () {
-    $("#btnNivel").click(function () {
-      if ($(this).hasClass("iniciante")) {
-        $(this)
-          .removeClass("iniciante")
-          .addClass("intermediario")
-          .text("Intermediário");
-      } else if ($(this).hasClass("intermediario")) {
-        $(this)
-          .removeClass("intermediario")
-          .addClass("avancado")
-          .text("Avançado");
-      } else if ($(this).hasClass("avancado")) {
-        $(this)
-          .removeClass("avancado")
-          .addClass("especialista")
-          .text("Especialista");
-      } else if ($(this).hasClass("especialista")) {
-        $(this)
-          .removeClass("especialista")
-          .addClass("iniciante")
-          .text("Iniciante");
-      }
-    });
+  $("#btnNivel").click(function () {
+    if ($(this).hasClass("iniciante")) {
+      $(this)
+        .removeClass("iniciante")
+        .addClass("intermediario")
+        .text("Intermediário");
+    } else if ($(this).hasClass("intermediario")) {
+      $(this)
+        .removeClass("intermediario")
+        .addClass("avancado")
+        .text("Avançado");
+    } else if ($(this).hasClass("avancado")) {
+      $(this)
+        .removeClass("avancado")
+        .addClass("especialista")
+        .text("Especialista");
+    } else if ($(this).hasClass("especialista")) {
+      $(this)
+        .removeClass("especialista")
+        .addClass("iniciante")
+        .text("Iniciante");
+    }
   });
-  
+});
+
+$(document).ready(function () {
+  $("#btnNivel2").click(function () {
+    if ($(this).hasClass("iniciante")) {
+      $(this)
+        .removeClass("iniciante")
+        .addClass("intermediario")
+        .text("Intermediário");
+    } else if ($(this).hasClass("intermediario")) {
+      $(this)
+        .removeClass("intermediario")
+        .addClass("avancado")
+        .text("Avançado");
+    } else if ($(this).hasClass("avancado")) {
+      $(this)
+        .removeClass("avancado")
+        .addClass("especialista")
+        .text("Especialista");
+    } else if ($(this).hasClass("especialista")) {
+      $(this)
+        .removeClass("especialista")
+        .addClass("iniciante")
+        .text("Iniciante");
+    }
+  });
+});
+$(document).ready(function () {
+  $("#btnNivel3").click(function () {
+    if ($(this).hasClass("iniciante")) {
+      $(this)
+        .removeClass("iniciante")
+        .addClass("intermediario")
+        .text("Intermediário");
+    } else if ($(this).hasClass("intermediario")) {
+      $(this)
+        .removeClass("intermediario")
+        .addClass("avancado")
+        .text("Avançado");
+    } else if ($(this).hasClass("avancado")) {
+      $(this)
+        .removeClass("avancado")
+        .addClass("especialista")
+        .text("Especialista");
+    } else if ($(this).hasClass("especialista")) {
+      $(this)
+        .removeClass("especialista")
+        .addClass("iniciante")
+        .text("Iniciante");
+    }
+  });
+});
+
+$(document).ready(function () {
+  $("#btnNivel4").click(function () {
+    if ($(this).hasClass("iniciante")) {
+      $(this)
+        .removeClass("iniciante")
+        .addClass("intermediario")
+        .text("Intermediário");
+    } else if ($(this).hasClass("intermediario")) {
+      $(this)
+        .removeClass("intermediario")
+        .addClass("avancado")
+        .text("Avançado");
+    } else if ($(this).hasClass("avancado")) {
+      $(this)
+        .removeClass("avancado")
+        .addClass("especialista")
+        .text("Especialista");
+    } else if ($(this).hasClass("especialista")) {
+      $(this)
+        .removeClass("especialista")
+        .addClass("iniciante")
+        .text("Iniciante");
+    }
+  });
+});
