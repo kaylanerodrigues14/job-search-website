0acb6a1fbc4c90ee5a42383d4b282edd6eb5fabb
kaylanerodrigues14 <148080549+kaylanerodrigues14@users.noreply.github.com>
2024-01-16 19:57:19 +0000

    Update 3 files

---
+++ js/skills.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $("#btnNivel").click(function () {
+      if ($(this).hasClass("iniciante")) {
+        $(this)
+          .removeClass("iniciante")
+          .addClass("intermediario")
+          .text("Intermediário");
+      } else if ($(this).hasClass("intermediario")) {
+        $(this)
+          .removeClass("intermediario")
+          .addClass("avancado")
+          .text("Avançado");
+      } else if ($(this).hasClass("avancado")) {
+        $(this)
+          .removeClass("avancado")
+          .addClass("especialista")
+          .text("Especialista");
+      } else if ($(this).hasClass("especialista")) {
+        $(this)
+          .removeClass("especialista")
+          .addClass("iniciante")
+          .text("Iniciante");
+      }
+    });
+  });
+  
